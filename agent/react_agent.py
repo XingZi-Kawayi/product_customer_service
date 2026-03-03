221a02dd8b8c47886126e1213f72287c8d943764
XingZi-Kawayi <1023440984@qq.com>
2026-03-03 20:19:35 +0800

    feat: 添加记忆管理支持到 ReactAgent

--- agent/react_agent.py
+++ agent/react_agent.py
@@ -4,10 +4,11 @@ from utils.prompt_loader import load_system_prompts
 from agent.tools.agent_tools import (rag_summarize, get_weather, get_user_location, get_user_id,
                                      get_current_month, fetch_external_data, fill_context_for_report)
 from langchain_core.messages import AIMessageChunk
+from memory.memory_manager import MemoryManager
 
 
 class ReactAgent:
-    def __init__(self):
+    def __init__(self, memory_manager: MemoryManager = None):
         chat_model = get_chat_model()
         if chat_model is None:
             raise RuntimeError("请在环境变量中设置 DASHSCOPE_API_KEY")
@@ -17,14 +18,20 @@ class ReactAgent:
                    get_current_month, fetch_external_data, fill_context_for_report],
         )
         self.system_prompt = load_system_prompts()
+        self.memory = memory_manager
 
-    def execute_stream(self, query: str):
-        input_dict = {
-            "messages": [
-                {"role": "system", "content": self.system_prompt},
-                {"role": "user", "content": query},
-            ]
-        }
+    def execute_stream(self, query: str, use_memory: bool = True):
+        if self.memory and use_memory:
+            messages = self.memory.get_langchain_messages(system_prompt=self.system_prompt)
+            messages.append({"role": "user", "content": query})
+            input_dict = {"messages": messages}
+        else:
+            input_dict = {
+                "messages": [
+                    {"role": "system", "content": self.system_prompt},
+                    {"role": "user", "content": query},
+                ]
+            }
 
         for event in self.agent.stream(input_dict, stream_mode="updates", context={"report": False}):
             for node_name, node_output in event.items():
@@ -34,6 +41,9 @@ class ReactAgent:
                             yield msg.content
                         elif hasattr(msg, "content") and msg.content and not isinstance(msg, AIMessageChunk):
                             yield msg.content
+
+    def set_memory(self, memory_manager: MemoryManager):
+        self.memory = memory_manager
 
 
 if __name__ == '__main__':
